30b9490af2fc077b2d13df6730e9ba2ab2c45b11
Zachary King <zchrykng@fastmail.com>
2019-05-03 18:51:22 +0000

    initial types and templates

--- caster.go
+++ caster.go
@@ -1,5 +1,25 @@
 package gocaster
 
-import (
-  "github.com/dhowden/tag"
-)
+import "time"
+
+type Feed struct {
+	Title       string
+	Link        string
+	Description string
+	Episodes    []Episode
+}
+
+type Episode struct {
+	Title string
+	File  File
+}
+
+type File struct {
+	URL          string
+	Name         string
+	Artist       string
+	ModifiedTime time.Time
+	Duration     time.Duration
+	Type         string
+	Size         int
+}
